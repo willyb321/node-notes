822866cc17e72ab2f188cc9b24de2c840fb1e445
willyb321 <wbwilliam7@gmail.com>
2016-07-17 14:25:07 -0700

    grumble linting

--- cli.js
+++ cli.js
@@ -3,11 +3,11 @@
 
 const fs = require('fs');
 
+const os = require('os');
+
+const path = require('path');
+
 const prompt = require('prompt');
-
-const os = require('os')
-
-const path = require('path')
 
 prompt.start();
 var schema = {
@@ -21,27 +21,35 @@ var schema = {
 };
 console.log('What do you want to do?\n1. Read Notes.\n2. Take notes.\n3. Reset Notes');
 prompt.get(schema, function (err, result) {
-	if (err) throw err;
+	if (err) {
+		throw err;
+	}
 	if (result.firstChoice === '1' || result.firstChoice === 1) {
 		if (err) {
-			console.log('ERROR!');
+			throw err;
 		}
 		fs.readFile(path.join(os.homedir(), 'notes.txt'), 'utf8', (err, notes) => {
-			if (err) throw err;
+			if (err) {
+				throw err;
+			}
 			console.log(notes);
 		});
 	}
 	if (result.firstChoice === '2' || result.firstChoice === 2) {
 		console.log('What is your note?');
 		prompt.get(['note'], function (err, result) {
-			if (err) throw err
+			if (err) {
+				throw err;
+			}
 			console.log('Your note: ' + result.note);
 			fs.appendFile(path.join(os.homedir(), 'notes.txt'), result.note + '\n');
 		});
 	}
 	if (result.firstChoice === '3' || result.firstChoice === 3) {
 		fs.unlink(path.join(os.homedir(), 'notes.txt'), err => {
-			if (err) throw err
+			if (err) {
+				throw err;
+			}
 			if (!err) {
 				console.log('Reset your notes.');
 			}
